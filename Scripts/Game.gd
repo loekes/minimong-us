0da67ef46f47297333c0412cd031e7f0e0da473d
Lukas Raymaekers <lukas.raymaekers@student.howest.be>
2021-04-15 17:24:49 +0200

    added a shader to transition between secret passageway and castle

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -4,6 +4,7 @@ onready var world = $World
 onready var ground_floor = $"World/Ground Floor"
 onready var basement = $World/Basement
 onready var player = $Player
+onready var transition_player = $"UI/Circle Transition/AnimationPlayer"
 
 var removed_node
 
@@ -24,5 +25,12 @@ func _on_Trapdoor_teleport_to_world(world_position):
 func teleport(from):
 	var node_to_add = removed_node
 	removed_node = from
+	
+	transition_player.play("close")
+	yield(transition_player, "animation_finished")
+	
 	world.remove_child(removed_node)
 	world.add_child(node_to_add)
+	
+	transition_player.play("open")
+	yield(transition_player, "animation_finished")
